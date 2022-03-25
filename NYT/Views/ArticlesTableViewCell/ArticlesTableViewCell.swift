beb80ffe307ab080835d7032f185d83fcc8d6eca
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-26 00:24:05 +0200

    refactored to make Views Group

---
+++ NYT/Views/ArticlesTableViewCell/ArticlesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticlesTableViewCell.swift
+//  NYT
+//
+//  Created by Abdulrahman on 3/26/22.
+//
+
+import UIKit
+
+class ArticlesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
