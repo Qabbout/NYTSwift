4e32e1af28a8296160cc5c413ef2a9ea5e7a8e91
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-26 23:21:00 +0200

    added DetailsViewController

---
+++ NYT/Controllers/DetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.swift
+//  NYT
+//
+//  Created by Abdulrahman on 3/26/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var detailsLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
